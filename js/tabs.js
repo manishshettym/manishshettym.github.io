3825f3c6b99e6578fee7e10931f969408e8f4942
ManishShettyM <mmshetty.98@gmail.com>
2021-09-07 11:07:53 +0530

    MAJOR UPDATE

--- js/tabs.js
+++ js/tabs.js
@@ -3,25 +3,20 @@ $(function () {
     $(".news").load("sections/news.html");
     $(".service").load("sections/service.html");
     $(".experience").load("sections/experience.html");
-    $(".publications").load("sections/publications.html");
 
-    var ns = document.querySelector('.news'),
-        ac = document.querySelector('.service'),
+    var ac = document.querySelector('.service'),
         ex = document.querySelector('.experience');
 
-    ns.style.display = 'none';
     ac.style.display = 'none';
     ex.style.display = 'none';
 });
 
 function switchTab(tab) {
-    var pb = document.querySelector('.publications'),
-        ns = document.querySelector('.news'),
+    var ns = document.querySelector('.news'),
         ac = document.querySelector('.service'),
         ex = document.querySelector('.experience');
 
-    var pbt = document.querySelector('.pb-tab'),
-        nst = document.querySelector('.ns-tab'),
+    var nst = document.querySelector('.ns-tab'),
         act = document.querySelector('.ac-tab'),
         ext = document.querySelector('.ex-tab');
 
@@ -29,18 +24,6 @@ function switchTab(tab) {
         case 'ns':
             ns.style.display = 'block';
             nst.className = 'tab ns-tab active';
-            pb.style.display = 'none';
-            pbt.className = 'tab pb-tab';
-            ex.style.display = 'none';
-            ext.className = 'tab ex-tab';
-            ac.style.display = 'none';
-            act.className = 'tab ac-tab';
-            break;
-        case 'pb':
-            ns.style.display = 'none';
-            nst.className = 'tab ns-tab';
-            pb.style.display = 'flex';
-            pbt.className = 'tab pb-tab active';
             ex.style.display = 'none';
             ext.className = 'tab ex-tab';
             ac.style.display = 'none';
@@ -49,8 +32,6 @@ function switchTab(tab) {
         case 'ex':
             ns.style.display = 'none';
             nst.className = 'tab ns-tab';
-            pb.style.display = 'none';
-            pbt.className = 'tab pb-tab';
             ex.style.display = 'block';
             ext.className = 'tab ex-tab active';
             ac.style.display = 'none';
@@ -59,8 +40,6 @@ function switchTab(tab) {
         case 'ac':
             ns.style.display = 'none';
             nst.className = 'tab ns-tab';
-            pb.style.display = 'none';
-            pbt.className = 'tab pb-tab';
             ex.style.display = 'none';
             ext.className = 'tab ex-tab';
             ac.style.display = 'block';
